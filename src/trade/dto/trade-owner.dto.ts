3f63548fc11780eabd5c4030a13db59512447d74
Otonye Iyalla <zytronglobal@gmail.com>
2026-06-12 14:19:29 +0200

    Allow authenticated trade owner query omission

--- src/trade/dto/trade-owner.dto.ts
+++ src/trade/dto/trade-owner.dto.ts
@@ -1,6 +1,7 @@
-import { IsUUID } from 'class-validator';
+import { IsOptional, IsUUID } from 'class-validator';
 
 export class TradeOwnerDto {
+  @IsOptional()
   @IsUUID()
-  userId: string;
+  userId?: string;
 }
